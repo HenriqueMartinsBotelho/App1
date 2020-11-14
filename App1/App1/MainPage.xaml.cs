656080883238548bb9ce58cdbdd2f97c2d2821dc
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-11-14 15:03:09 -0300

    app01 concluído

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using App1.Servicos.Modelo;
+using App1.Servicos;
 
 namespace App1
 {
@@ -13,6 +15,12 @@ namespace App1
         public MainPage()
         {
             InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+        private void BuscarCEP(object sender, EventArgs args) {
+            string cep = CEP.Text.Trim();
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+            RESULTADO.Text = string.Format("Endereco: {3} {0}, {1}, {2} ", end.localidade, end.uf, end.logradouro, end.bairro);
         }
     }
 }
---
+++ App1/App1/Servicos/ViaCEPServico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1.Servicos.Modelo;
+using Newtonsoft.Json;
+
+namespace App1.Servicos
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo)  ;
+            return end; 
+        }
+    }
+}
